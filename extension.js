0f15161308c8985c8f84f4c2ed5aa60d66925e0c
r3gz3n <akashmailbox722@gmail.com>
2019-10-29 12:15:52 +0530

    Added functionality to get data from competitive companion

--- extension.js
+++ extension.js
@@ -1,9 +1,47 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function startServer() {
+	// This function will start the server and listen to the given port
+	// Make sure port is same as mention in the Competitive Companion extension in your browser
+
+	const app = require('express')();
+	const bodyParser = require('body-parser');
+
+	const port = 8080;
+
+	app.use(bodyParser.json());
+
+	app.post('/', (req, res) => {
+		const data = req.body;
+
+		console.log('Full body:');
+		console.log(JSON.stringify(data, null, 4));
+
+		res.sendStatus(200);
+	});
+
+	app.listen(port, err => {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+
+		console.log(`Listening on port ${port}`);
+	});
+}
+
+
+function startCompetitiveCompanionServer() {
+	try {
+		startServer();
+		vscode.window.showInformationMessage("Contest Helper: Server started successfully!!!");
+	}
+	catch (exception) {
+		console.log(exception);
+		vscode.window.showErrorMessage("Contest Helper: Server failed to start!!!");
+	}
+}
+
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -17,14 +55,9 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	startCompetitiveCompanionServer();
+	// let disposable = vscode.commands.registerCommand('extension.startCompetitiveCompanionServer', startCompetitiveCompanionServer);
+	// context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
