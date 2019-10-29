e68870ae5ed5a6f13cbd1e872d12c8f81079f475
r3gz3n <akashmailbox722@gmail.com>
2019-10-29 15:03:29 +0530

    Added listener that will listen to competitive companion

--- extension.js
+++ extension.js
@@ -1,45 +1,19 @@
 const vscode = require('vscode');
-
-function startServer() {
-	// This function will start the server and listen to the given port
-	// Make sure port is same as mention in the Competitive Companion extension in your browser
-
-	const app = require('express')();
-	const bodyParser = require('body-parser');
-
-	const port = 8080;
-
-	app.use(bodyParser.json());
-
-	app.post('/', (req, res) => {
-		const data = req.body;
-
-		console.log('Full body:');
-		console.log(JSON.stringify(data, null, 4));
-
-		res.sendStatus(200);
-	});
-
-	app.listen(port, err => {
-		if (err) {
-			console.error(err);
-			process.exit(1);
-		}
-
-		console.log(`Listening on port ${port}`);
-	});
-}
+const listenerConstructor = require('./src/listenerToCompetitiveCompanion.js');
 
 
 function startCompetitiveCompanionServer() {
 	try {
-		startServer();
-		vscode.window.showInformationMessage("Contest Helper: Server started successfully!!!");
+		listenerConstructor();
+		console.log("Listener started successfully!!!");
+		vscode.window.showInformationMessage("ContestHelper: Listener started successfully!!!");
 	}
 	catch (exception) {
-		console.log(exception);
-		vscode.window.showErrorMessage("Contest Helper: Server failed to start!!!");
+		console.error(exception);
+		vscode.window.showErrorMessage("ContestHelper: Listener failed to start!!!");
+		process.exit(1);
 	}
+	
 }
 
 
@@ -47,17 +21,9 @@ function startCompetitiveCompanionServer() {
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "contesthelper" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
+	// Activating ContestHelper
+	console.log('Activating ContestHelper...');
 	startCompetitiveCompanionServer();
-	// let disposable = vscode.commands.registerCommand('extension.startCompetitiveCompanionServer', startCompetitiveCompanionServer);
-	// context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
